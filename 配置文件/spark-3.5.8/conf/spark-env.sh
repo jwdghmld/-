33c1953b3d14bfc06332f717e3bb5125fcabdf4b
jwdghmld <2517749950@qq.com>
2026-05-13 10:30:57 +0800

    Add files via upload
    各组件配置文件【可选】

---
+++ 配置文件/spark-3.5.8/conf/spark-env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export HADOOP_CONF_DIR=/opt/app/hadoop-3.3.6/etc/hadoop
+export YARN_CONF_DIR=/opt/app/hadoop-3.3.6/etc/hadoop
+export PYSPARK_PYTHON=你的python路径
+export PYSPARK_DRIVER_PYTHON=你的python路径
